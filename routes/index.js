d2eaf5eb36e7f042fbe6af529a6fdbe26c8f74e3
bovetliu <bowei_liu@ymail.com>
2015-07-02 22:56:41 -0500

    listing.easysublease.com registered

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'new easysublease.org listing page demo',
-    online_sample:"55910d94008d84ed2912e1ae",
+    online_sample:"559418e1731b61add4b8d532",
     local_sample:"5593aa9e48f2890a9a0f2929"  
    });
 });
