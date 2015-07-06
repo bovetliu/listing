41eb7b6d7a76fb7b87736eef39f57381b233eb09
bovetliu <bowei_liu@ymail.com>
2015-07-05 22:01:36 -0500

    js fix ul li height 100% problem

--- public/javascripts/howitworks_control.js
+++ public/javascripts/howitworks_control.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+  //resolve the height problem
+  //virtual CSS problems fix, I need to keep js code here as little as possible !
+  $(".slide").height($(window).height() );
+  $(window).on('resize', function(){
+    $(".slide").height($(window).height() );
+  });
+
+
 // ready() call back now
   /* ======= Header Background Slideshow - Flexslider ======= */    
   /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties */
-  
+  //console.log($(window).height() );
+
+
   $('.bg-slider').flexslider({
       animation: "slide",
       directionNav: false, //remove the default direction-nav - https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
